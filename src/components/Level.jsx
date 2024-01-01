f3e7053659408b7e5e57238eba077183fc07c2af
RUO <qqluolucy@gmail.com>
2024-01-02 01:15:09 +0800

    feat: complete Level component

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -2,76 +2,67 @@ import { imgSrc } from "@/utils/imgSrc";
 import levelsJson from "@/resources/level.json";
 
 export default function Level(props) {
-  console.log("here");
   const { scores, setScores } = props ?? {};
   if (!scores || !setScores) return null;
-  console.log("there");
 
-  const index = scores.reduce((sum, score) => sum + score);
+  const levelIndex = scores.reduce((sum, score) => sum + score);
+  const questionNumber = "Q" + (levelIndex + 1);
+  const level = levelsJson[levelIndex];
 
-  const level = levelsJson[index];
+  const onOptionClick = (optionIndex) => {
+    scores[optionIndex]++;
+    setScores([...scores]);
+  };
 
   return (
-    <div
-      onClick={() => {
-        scores[0]++;
-        setScores([...scores]);
-      }}
-    >
-      Level: {index}
+    <div className="relative max-w-[450px] h-screen mx-auto flex-col flex items-center">
+      <img
+        src={imgSrc(level.levelBgImg)}
+        className="absolute h-full w-full object-cover object-center inset-0"
+      />
+      <img
+        src={imgSrc(level.decorationTopLineImg)}
+        className="object-contain object-center w-[90%] absolute top-[12px] left-0 right-0 mx-auto"
+      />
+      <div className="relative text-primary text-base font-bold tracking-widest mt-[50px]">
+        {level.story.title}
+      </div>
+      <div className="w-full absolute bottom-[60px] flex flex-col items-center">
+        <div className="flex-col relative justify-center items-stretch overflow-hidden flex aspect-[5.27] w-[90%] p-4 pb-5">
+          <img
+            src={imgSrc(level.questionBlockBgImg)}
+            className="absolute h-full w-full object-cover object-center inset-0"
+          />
+          <div className="relative text-primary flex justify-between gap-2 cursor-default">
+            <div className="text-4xl whitespace-nowrap">{questionNumber}</div>
+            <div className="text-sm font-bold tracking-tight self-center grow shrink basis-auto my-auto">
+              {level.story.question}
+            </div>
+          </div>
+        </div>
+        <ul className="flex flex-col gap-2 list-roman mt-2">
+          {level.story.options.map((option, index) => (
+            <button
+              key={option.key}
+              className="relative block w-[317px] h-[36px] border-[0.5px] pl-10 border-orange-200 cursor-pointer"
+              onClick={() => onOptionClick(index)}
+            >
+              <img
+                src={imgSrc(level.optionBlockBgImg)}
+                alt=""
+                className="w-full h-full absolute left-0 top-0 object-cover object-center aspect-[8.8]"
+              />
+              <li className="relative z-10 text-secondary text-left text-xs">
+                {option.content}
+              </li>
+            </button>
+          ))}
+        </ul>
+      </div>
+      <img
+        src={imgSrc(level.decorationBottomLineImg)}
+        className="object-contain object-center w-[90%] absolute bottom-[10px] left-0 right-0 mx-auto"
+      />
     </div>
-    // <div className="relative max-w-[450px] h-screen mx-auto flex-col flex items-center">
-    //   <img
-    //     src={imgSrc(level.levelBgImg)}
-    //     className="absolute h-full w-full object-cover object-center inset-0"
-    //   />
-    //   <img
-    //     loading="lazy"
-    //     src={imgSrc(level.decorationTopLineImg)}
-    //     className="object-contain object-center w-[90%] absolute top-[12px] left-0 right-0 mx-auto"
-    //   />
-    //   <div className="relative text-orange-200 text-base font-bold tracking-widest mt-[50px]">
-    //     {level.story.title}
-    //   </div>
-    //   <div className="w-full absolute bottom-[60px] flex flex-col items-center">
-    //     <div className="flex-col relative justify-center items-stretch overflow-hidden flex aspect-[5.276923076923077] w-[90%] p-4 pb-5">
-    //       <img
-    //         src={imgSrc(level.questionBlockBgImg)}
-    //         className="absolute h-full w-full object-cover object-center inset-0"
-    //       />
-    //       <div className="relative items-stretch flex justify-between gap-2">
-    //         <div className="text-orange-200 text-4xl whitespace-nowrap">Q1</div>
-    //         <div className="text-orange-200 text-sm font-bold tracking-tight self-center grow shrink basis-auto my-auto">
-    //           {level.story.question}
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <ul>
-    //       {level.story.options.map((option) => (
-    //         <div
-    //           key={option.key}
-    //           className="flex-col relative items-stretch overflow-hidden flex aspect-[8.805555555555555] w-[317px] max-w-[317px] justify-center mt-2 border-[0.5px] border-solid border-orange-200"
-    //         >
-    //           <img
-    //             src={imgSrc(level.optionBlockBgImg)}
-    //             className="absolute h-full w-full object-cover object-center inset-0"
-    //           />
-    //           <div className="relative justify-between items-center flex gap-0 p-2.5">
-    //             <div className="text-yellow-900 text-xs tracking-tighter whitespace-nowrap my-auto">
-    //               Ⅰ.
-    //             </div>
-    //             <div className="text-yellow-900 text-xs font-bold self-stretch grow whitespace-nowrap">
-    //               {option.content}
-    //             </div>
-    //           </div>
-    //         </div>
-    //       ))}
-    //     </ul>
-    //   </div>
-    //   <img
-    //     src={imgSrc(level.decorationBottomLineImg)}
-    //     className="object-contain object-center w-[90%] absolute bottom-[10px] left-0 right-0 mx-auto"
-    //   />
-    // </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#e2d99e",
+      secondary: "#60482c",
+    },
+    listStyleType: {
+      roman: "upper-roman",
+    },
   },
   plugins: [],
 };
